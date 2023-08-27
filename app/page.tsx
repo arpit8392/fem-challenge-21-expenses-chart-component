8e5b44a772b8ca59c22fe0d58779af1e2746a518
Arpit Namdev <arpit8392@gmail.com>
2023-08-27 15:33:01 +0530

    Conditional Styling based on Maximum Value of Data Point + Styling as per the design

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import ChartSummary from '@/components/chart-summary'
 export default function Home() {
 	return (
 		<main className='h-screen lg:grid place-content-center'>
-			<div className='px-4 space-y-4 py-16 lg:w-[540px]'>
+			<div className='px-4 space-y-4 lg:space-y-6 py-16 lg:w-[540px]'>
 				<ChartSummary title='My balance' value='$921.48' />
-        <ExpenseBarChart />
+				<ExpenseBarChart />
 			</div>
 		</main>
 	)
--- components/bar-chart.tsx
+++ components/bar-chart.tsx
@@ -1,31 +1,85 @@
 'use client'
 
 import data from '@/data.json'
-import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts'
+import {
+	Bar,
+	BarChart,
+	Cell,
+	ResponsiveContainer,
+	Tooltip,
+	XAxis,
+} from 'recharts'
+
+interface Payload {
+	name: string
+	value: string
+}
+interface CustomToolTipProps {
+	active?: boolean
+	label?: string
+	payload?: Payload[]
+}
+const CustomTooltip = ({ active, payload }: CustomToolTipProps) => {
+	if (active && payload && payload.length) {
+		return (
+			<div className='p-2 bg-darkBrown rounded-md text-paleOrange'>
+				<p className='text-lg font-bold'>${payload[0].value}</p>
+			</div>
+		)
+	}
+
+	return null
+}
+
+const getMaxValue = () => {
+	return data.reduce((max, current) => {
+		return current.amount > max ? current.amount : max
+	}, -Infinity)
+}
 
 const ExpenseBarChart = () => {
+	const maxValue = getMaxValue()
+
 	return (
-		<div className='bg-paleOrange rounded-[20px] px-5 py-6 lg:px-10 lg:py-8'>
-			<h1 className='text-darkBrown text-2xl font-bold'>
+		<div className='bg-paleOrange rounded-xl px-5 py-6 lg:px-10 lg:py-8'>
+			<h1 className='text-darkBrown text-2xl font-bold lg:text-3xl'>
 				Spending - Last 7 Days
 			</h1>
-			<ResponsiveContainer width='100%' height={350}>
-				<BarChart data={data}>
-					<XAxis
-						dataKey='day'
-						stroke='#888888'
-						fontSize={12}
-						tickLine={false}
-						axisLine={false}
-					/>
-					<Tooltip />
-					<Bar dataKey='amount' fill='#EC755D' radius={[5, 5, 5, 5]} />
-				</BarChart>
-			</ResponsiveContainer>
+			<div className='mt-14 lg:mt-16'>
+				<ResponsiveContainer width='100%' height={250}>
+					<BarChart data={data}>
+						<XAxis
+							dataKey='day'
+							fontSize={12}
+							tickLine={false}
+							axisLine={false}
+						/>
+						<Tooltip content={<CustomTooltip />} />
+						<Bar
+							dataKey='amount'
+							radius={[5, 5, 5, 5]}
+							className='cursor-pointer'>
+							{data.map((item, index) => (
+								<Cell
+									key={`cell-${index}`}
+									className={
+										item.amount === maxValue
+											? 'fill-cyan hover:fill-[#B4E0E5]'
+											: 'fill-softRed hover:fill-[#FF9B86]'
+									}
+								/>
+							))}
+						</Bar>
+					</BarChart>
+				</ResponsiveContainer>
+			</div>
+
 			<hr className='h-[2px] rounded-[1px] my-6 lg:my-8 bg-cream' />
 			<div className='flex justify-between'>
 				<div className='space-y-1'>
-					<p className='text-sm text-mediumBrown lg:text-lg'>Total this month</p>
+					<p className='text-sm text-mediumBrown lg:text-lg'>
+						Total this month
+					</p>
 					<p className='font-bold text-3xl lg:text-5xl'>$478.33</p>
 				</div>
 				<div className='text-right self-end'>
